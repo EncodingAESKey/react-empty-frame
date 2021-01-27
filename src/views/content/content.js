85487eca1e1e5917ddeaa60b354b5fa9784ae200
wangwenli4 <1421247916@qq.com>
2021-01-27 14:36:59 +0800

    初始化项目

---
+++ src/views/content/content.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import './index.less'
+
+class Login extends Component {
+  render(){
+    return (
+      <div className='content-page'>
+          主页面
+      </div>
+    )
+  }
+}
+export default Content;
---
+++ src/views/login/index.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import './index.less'
+
+class Login extends Component {
+  render(){
+    return (
+      <div className='login-page'>
+           <div className="zhyw-logo"></div>
+      </div>
+    )
+  }
+}
+export default Login;
